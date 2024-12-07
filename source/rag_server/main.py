0b93e833caceaafba715ee57e4100f8fb7b31778
dodoga <fnzmwpekdl@naver.com>
2024-12-07 13:04:24 +0900

    정상화

--- source/rag_server/main.py
+++ source/rag_server/main.py
@@ -1,8 +1,6 @@
 import os
-import streamlit as st
 from app.chat_app import ChatApp
 
-@st.cache_resource
 def load_app():
     current_dir = os.getcwd()
 
@@ -12,7 +10,6 @@ def load_app():
     else:
         document_dir = f"/mount/src/{project_name}/document"
     
-    print("여기도 다시 init?")
     return ChatApp({
         "llm_url": "https://complete-mastiff-special.ngrok-free.app/llm/",
         "document_dir": document_dir,
